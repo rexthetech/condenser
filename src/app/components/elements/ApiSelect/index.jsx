e5f79fbabab7f44eab05995fd5c78159d7f2df07
rexthetech <rextoledo8@gmail.com>
2022-11-03 12:38:23 +0000

    CI

--- src/app/components/elements/ApiSelect/index.jsx
+++ src/app/components/elements/ApiSelect/index.jsx
@@ -4,7 +4,28 @@ import PropTypes from 'prop-types';
 const ApiSelect = () => {
     function eventTest(e) {
         e.preventDefault();
-        console.log('Event fired!');
+        console.log('**** mark: in');
+
+        if (typeof steem !== 'undefined')
+            console.log('steem in scope');
+
+
+        if (typeof config !== 'undefined')
+            console.log('config in scope');
+/*
+        steem.api.setOptions({
+            url: config.steemd_connection_server,
+            retry: {
+                retries: 10,
+                factor: 5,
+                minTimeout: 50,
+                maxTimeout: 60 * 1000,
+                randomize: true,
+            },
+            useAppbaseApi: !!config.steemd_use_appbase,
+        });
+*/
+        console.log('**** mark: out');
     }
 
     return <div className="NodeSelect row" onClick={eventTest}>Placeholder</div>;
