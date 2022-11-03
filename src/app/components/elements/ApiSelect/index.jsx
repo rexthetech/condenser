84e6517c22b407e14c959f64f7824eca01f69027
rexthetech <rextoledo8@gmail.com>
2022-11-03 12:30:36 +0000

    CI

--- src/app/components/elements/ApiSelect/index.jsx
+++ src/app/components/elements/ApiSelect/index.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const ApiSelect = () => {
-    return <div className="NodeSelect row">Placeholder</div>;
+    function eventTest(e) {
+        e.preventDefault();
+        console.log('Event fired!');
+    }
+
+    return <div className="NodeSelect row" onclick={eventTest}>Placeholder</div>;
 };
 
 export default ApiSelect;
